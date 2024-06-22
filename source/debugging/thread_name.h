de1e783b5e0482c2433977f098f3186c2d2ae153
Ali Salehi <nysalehi@gmail.com>
2024-06-22 09:32:55 +0200

    Make setThreadName inline.

--- source/debugging/thread_name.h
+++ source/debugging/thread_name.h
@@ -2,15 +2,15 @@
 
 namespace Immortals::Common::Debug
 {
-void setThreadName(const char *name)
+inline void setThreadName(const char *name)
 {
 #if defined(_WIN32)
-    const std::wstring wideName{name, name + strlen(name)};
+    const std::wstring wide_name{name, name + strlen(name)};
 
-    const HRESULT hr = SetThreadDescription(GetCurrentThread(), wideName.c_str());
-    if (FAILED(hr))
+    const HRESULT result = SetThreadDescription(GetCurrentThread(), wide_name.c_str());
+    if (FAILED(result))
     {
-        logError("Failed to set thread name \"{}\": {}", name, hr);
+        logError("Failed to set thread name \"{}\": {}", name, result);
     }
 #elif defined(__linux__) || defined(__APPLE__)
     pthread_setname_np(name);
